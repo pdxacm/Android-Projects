03fb75255a9c49db72297427b31148c886e888d1
Joe Yuhas <joe.yuhas@gmail.com>
2013-12-03 13:32:16 -0800

    created a raw folder in res/ to put txt files in

--- Team-1-Project/WindowReplacement/gen/org/acm/windowreplacement/R.java
+++ Team-1-Project/WindowReplacement/gen/org/acm/windowreplacement/R.java
@@ -9,9 +9,9 @@ package org.acm.windowreplacement;
 
 public final class R {
     public static final class array {
-        public static final int energy_source_string=0x7f060000;
-        public static final int window_frame_type_string_array=0x7f060002;
-        public static final int window_pane_type_string_array=0x7f060001;
+        public static final int energy_source_string=0x7f070000;
+        public static final int window_frame_type_string_array=0x7f070002;
+        public static final int window_pane_type_string_array=0x7f070001;
     }
     public static final class attr {
     }
@@ -22,64 +22,67 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int edit_text_dimen=0x7f040004;
-        public static final int text_view_dimen=0x7f040003;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int edit_text_dimen=0x7f050004;
+        public static final int text_view_dimen=0x7f050003;
         /**  MainActivity 
          */
-        public static final int text_view_long_dimen=0x7f040002;
+        public static final int text_view_long_dimen=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
-        public static final int editText1=0x7f090001;
-        public static final int energySpinner=0x7f090003;
-        public static final int heatTypeLabelTextView=0x7f090002;
-        public static final int monthlyCostEditText=0x7f090005;
-        public static final int monthlyCostTextView=0x7f090004;
-        public static final int nextButton=0x7f090006;
-        public static final int stateLabelTextView=0x7f090000;
-        public static final int windowFrameTypeSpinner=0x7f09000a;
-        public static final int windowFrameTypeTextView=0x7f090009;
-        public static final int windowPaneTypeSpinner=0x7f090008;
-        public static final int windowPaneTypeTextView=0x7f090007;
-        public static final int windowQuantityEditText=0x7f09000e;
-        public static final int windowQuantityTextView=0x7f09000d;
-        public static final int windowSizeTextEdit=0x7f09000c;
-        public static final int windowSizeTextView=0x7f09000b;
+        public static final int action_settings=0x7f0a000f;
+        public static final int editText1=0x7f0a0001;
+        public static final int energySpinner=0x7f0a0003;
+        public static final int heatTypeLabelTextView=0x7f0a0002;
+        public static final int monthlyCostEditText=0x7f0a0005;
+        public static final int monthlyCostTextView=0x7f0a0004;
+        public static final int nextButton=0x7f0a0006;
+        public static final int stateLabelTextView=0x7f0a0000;
+        public static final int windowFrameTypeSpinner=0x7f0a000a;
+        public static final int windowFrameTypeTextView=0x7f0a0009;
+        public static final int windowPaneTypeSpinner=0x7f0a0008;
+        public static final int windowPaneTypeTextView=0x7f0a0007;
+        public static final int windowQuantityEditText=0x7f0a000e;
+        public static final int windowQuantityTextView=0x7f0a000d;
+        public static final int windowSizeTextEdit=0x7f0a000c;
+        public static final int windowSizeTextView=0x7f0a000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_window_activity2=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int window_activity2=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int window_activity2=0x7f090001;
+    }
+    public static final class raw {
+        public static final int states=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int currentWindowPaneTypeString=0x7f05000a;
-        public static final int heatTypeString=0x7f050004;
-        public static final int monthlyCostHintString=0x7f050008;
-        public static final int monthlyCostString=0x7f050007;
-        public static final int monthlyExpenseString=0x7f050005;
-        public static final int nextButtonString=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int currentWindowPaneTypeString=0x7f06000a;
+        public static final int heatTypeString=0x7f060004;
+        public static final int monthlyCostHintString=0x7f060008;
+        public static final int monthlyCostString=0x7f060007;
+        public static final int monthlyExpenseString=0x7f060005;
+        public static final int nextButtonString=0x7f060006;
         /**  MainActivity 
          */
-        public static final int stateHintString=0x7f050002;
-        public static final int stateString=0x7f050003;
+        public static final int stateHintString=0x7f060002;
+        public static final int stateString=0x7f060003;
         /**  WindowActivity2 
          */
-        public static final int title_activity_window_activity2=0x7f050009;
-        public static final int windowFrameTypeString=0x7f05000b;
-        public static final int windowQuantityHintString=0x7f05000f;
-        public static final int windowQuantityString=0x7f05000e;
-        public static final int windowSizeHintString=0x7f05000d;
-        public static final int windowSizeString=0x7f05000c;
+        public static final int title_activity_window_activity2=0x7f060009;
+        public static final int windowFrameTypeString=0x7f06000b;
+        public static final int windowQuantityHintString=0x7f06000f;
+        public static final int windowQuantityString=0x7f06000e;
+        public static final int windowSizeHintString=0x7f06000d;
+        public static final int windowSizeString=0x7f06000c;
     }
     public static final class style {
         /** 
@@ -103,10 +106,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }

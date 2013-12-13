d043932a89e9d5e63aec350e446c77ed1eb5cf7e
Joe Yuhas <joe.yuhas@gmail.com>
2013-12-12 17:22:41 -0800

    added onclick listener and initialized a button object for activity two

--- Team-1-Project/WindowReplacement/src/org/acm/windowreplacement/WindowActivity2.java
+++ Team-1-Project/WindowReplacement/src/org/acm/windowreplacement/WindowActivity2.java
@@ -2,15 +2,21 @@ package org.acm.windowreplacement;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class WindowActivity2 extends Activity {
 
 	Spinner windowPaneTypeSpinner = null;
 	Spinner windowFrameTypeSpinner = null;
-	
+	Button nextButton = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,6 +34,10 @@ public class WindowActivity2 extends Activity {
 				R.array.window_frame_type_string_array, android.R.layout.simple_spinner_item);
 		frameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		windowFrameTypeSpinner.setAdapter(frameAdapter);
+		
+		nextButton = (Button) findViewById(R.id.nextButton);
+		
+		setButtonOnClickListeners();
 	}
 
 	@Override
@@ -35,6 +45,19 @@ public class WindowActivity2 extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.window_activity2, menu);
 		return true;
+	}
+	
+public void setButtonOnClickListeners(){
+		
+		nextButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				
+			
+	
+			}});
+		
 	}
 
 }
